d136e6b2d9804e5346ee9d7311e11c8345bc8658
Hrishabh Bhardwaj <121423516+bhardwaj-hrishabh@users.noreply.github.com>
2024-02-07 20:53:50 +0530

    Day-37

---
+++ Questions/44_Group Anagrams.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/group-anagrams/description/?envType=daily-question&envId=2024-02-06
+
+
+
+class Solution {
+public:
+
+    // For 2nd method
+    std::array<int,256> hash(string s){
+        std::array<int, 256>hash = {0};
+        for(int i =0;i<s.size();i++){
+            hash[s[i]]++;
+        }
+        return hash;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // METHOD - 1
+        // map<string, vector<string>> mp;
+
+        // for(auto str:strs){
+        //     string s = str;
+        //     sort(s.begin(), s.end());
+        //     mp[s].push_back(str);
+        // }
+
+        // vector<vector<string>>ans;
+        // for(auto it = mp.begin(); it!=mp.end(); it++){
+        //     ans.push_back(it->second);
+        // }
+        // return ans;
+
+
+
+        // METHOD - 2
+        map<std::array<int, 256>, vector<string>>mp;
+        
+        for(auto str:strs){
+            mp[hash(str)].push_back(str);
+        }
+
+        vector<vector<string>>ans;
+        for(auto it = mp.begin(); it!=mp.end(); it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+
+    }
+};
